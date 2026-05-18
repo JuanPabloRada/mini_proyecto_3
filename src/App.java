b46ea2d45fe807233d85c5d2224da2bee46b2a21
Joseph-PCV <josephandreypuerta@gmail.com>
2026-05-18 12:38:27 -0500

    - Construir la interfaz grafica para solicitar los nombres de los dos jugadores.  - Recolectar los nombres y delegar la acción de iniciar el duelo al  para que inicialice el modelo y la vista del duelo.

--- src/App.java
+++ src/App.java
@@ -1,8 +1,8 @@
 import javax.swing.SwingUtilities;
+import view.VentanaInicio;
 
 public class App {
     public static void main(String[] args) {
-        // Lanza la interfaz gráfica en el hilo de eventos de Swing
         SwingUtilities.invokeLater(() -> {
             VentanaInicio ventana = new VentanaInicio();
             ventana.setVisible(true);
